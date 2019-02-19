8dff1d327891b8736e6aaa541338782d274de54d
Michele Galvagno <michicello@gmail.com>
2019-02-19 22:01:31 +0100

    Challenge - question 2 - sorting the image names in the Table View

--- Storm Viewer/ViewController.swift
+++ Storm Viewer/ViewController.swift
@@ -26,6 +26,7 @@ class ViewController: UITableViewController {
                 // this is a picture to load!
                 pictures.append(item)
             }
+            pictures.sort()
         }
         
         print(pictures)
